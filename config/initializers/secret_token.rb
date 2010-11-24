83a1b2e0d8fbe694824f04adca61cfab5fb82d63
Markus Nordin <markus.nordin@gmail.com>
2010-11-24 11:27:06 +0100

    Initial commit, put info files in /doc plx

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Initech::Application.config.secret_token = 'fbf9d1895b3db6fcbb3fc787693f4fb05e9726d1e034b7997310e447cc61acd3dc688bc231359a08e692b3888ad33e845fd7cd261f3a27c6807d89918ef5231b'
