9f5902d5e139620b20e391858159ec8f3bdcdf3e
Chux <lechux@gmail.com>
2010-12-07 15:21:42 +0100

    Made a basic implementation of the design. Switched over so the project uses jquery instead of prototype. Also did a little routing ;)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Initech::Application.routes.draw do
 
   resources :surveys
 
+  root :to => "surveys#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
