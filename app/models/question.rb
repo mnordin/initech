0030beeb31b732f15a54481885a4d2e5a6670035
Markus Nordin <markus.nordin@gmail.com>
2010-12-07 15:58:18 +0100

    The survey admin edit works all the way down. But adding and deleting is still tricky stuff!

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :question_answering_alternatives
+  accepts_nested_attributes_for :question_answering_alternatives
+  
+  
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,16 @@
 class Survey < ActiveRecord::Base
   has_many :questions
+  accepts_nested_attributes_for :questions
   
   
+  def getQuestionTypes
+    { 
+      "textfield" => "textfield",
+      "select" => "select",
+      "textarea" => "textarea",
+      "radiobuttonlist" => "radiobuttonlist",
+      "checkboxlist" => "checkboxlist",
+      "scale" => "scale"
+    }
+  end
 end
