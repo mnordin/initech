6dc1a41a93b26cd99733985a46105385547f5422
Markus Nordin <markus.nordin@gmail.com>
2010-12-08 10:31:21 +0100

    merging survey

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(:version => 20101206090210) do
     t.string   "heading"
     t.datetime "send_date"
     t.datetime "deadline"
-    t.string   "status"
-    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
