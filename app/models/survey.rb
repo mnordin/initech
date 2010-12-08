bd13edf78b61b9bac0d965c10d90cbfd67825487
Chux <lechux@gmail.com>
2010-12-08 10:27:11 +0100

    Fixed a merge I totally messed uo

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,17 +1,10 @@
 class Survey < ActiveRecord::Base
   has_many :questions
-<<<<<<< HEAD
-  validates_presence_of :heading
   accepts_nested_attributes_for :questions, :allow_destroy => true
-  
-  
-=======
-  accepts_nested_attributes_for :questions
 
   validates :status, :presence => true
   validates :heading, :presence => true
 
->>>>>>> 9f21e2841e0e1a36601c3b4f8622deed4af504d7
   def getQuestionTypes
     { 
       "textfield" => "textfield",
